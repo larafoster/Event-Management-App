cdf4201362f1ad8183384e22132f16c506973af6
Gillian Baker <gillianbaker97@gmail.com>
2021-08-18 10:26:41 -0400

    seeds folder

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,10 @@
 const sequelize = require('../config/connection');
 const { User, Event } = require('../models');
 
-const userData = require('../models');
+
+const userData = require('./userData.json');
+const eventData = require('./eventData.json');
+
 
 const seedDatabase = async () => {
     await squelize.sync({ force: true});
@@ -12,13 +15,14 @@ const seedDatabase = async () => {
 
     });
 
-    await Event.bulkCreate(userData, { 
-        individualHooks: true,
-        returning: true,
+    for (const event of eventData) {
+        await Event.create({ 
+            ...event,
+            user_id: users[Math.floor(Math.random() * users.length)].id,
+        });
+    }
 
-    });
-    process.exit(0); 
-
+    process.exit(0);
 };
 
 
