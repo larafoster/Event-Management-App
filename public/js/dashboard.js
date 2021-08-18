81c777545262ef09d5c8f33b944ed8bcedeb48b9
Michael Carmody <mr.carmody89@gmail.com>
2021-08-18 11:43:16 -0400

    dashboard.js newFormHandler update

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,4 +1,46 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-}
+  const name = document.querySelector('#event-name').value.trim();
+  const description = document.querySelector('#event-desc').value.trim();
+
+  if (name && description) {
+    const response = await fetch(`/api/events`, {
+      method: 'POST',
+      body: JSON.stringify({ name, description }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/events');
+    } else {
+      alert('Failed to create event');
+    }
+  }
+};
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/events/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/events');
+    } else {
+      alert('Failed to delete event');
+    }
+  }
+};
+
+document
+  .querySelector('.new-event-form')
+  .addEventListener('submit', newFormHandler);
+
+document
+  .querySelector('.event-list')
+  .addEventListener('click', delButtonHandler);
