35bdfc412470edf412a48b39eb079f2805b6a385
Cameron H <cameronheadlee6@gmail.com>
2021-08-18 12:08:25 -0400

    adding some homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,14 +2,77 @@ const router = require('express').Router();
 const { Event, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
+router.get('./', async (req, res) => {
     try {
+        //GET all Events and JOIN with user data
         const eventData = await Event.findAll({
-            attributes: { exclude: ['password'] },
-            order: [['date', ]]
-        })
-    }
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                },
+            ],
+        });
+        //serialize data so the template can read it
+        const events = eventData.map((Event) => Event.get({plain: true}));
 
+        //Pass serialized data 
+        res.render('homepage', {
+            events,
+        });
+    } catch (err) {
+        res.status(500).json(err);    
+    }
 });
 
+router.get('./event/:id', withAuth)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+router.get('/dashboard', withAuth, async (req, res) => {
+    try{
+    
+     const userData = await User.findByPK(req.session.user_id, {
+         attributes: {exclude: ['password'] },
+         include: [{model: Event}],
+     });
+
+     const user = userData.get({ plain: true });
+
+     res.render('dashboard', {
+         ...user,
+         logged_in: true
+     });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+router.get('./login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('./dashboard');
+        return;
+    }
+
+    res.render('login');
+});
 module.exports = router;
