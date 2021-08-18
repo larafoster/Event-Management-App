d9fd5414408c9dd7536f93633192b46029276e15
lara <lara@vehicle4change.org>
2021-08-18 14:06:20 -0400

    event

--- controllers/api/eventRoutes.js
+++ controllers/api/eventRoutes.js
@@ -2,6 +2,14 @@ const router = require('express').Router();
 const { Event } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.
+router.get('/', async (req, res) => {
+    const eventData = await Event.findAll().catch((err) => {
+        res.json(err);
+    });
+
+    const events = eventData.map((event) => event.get({ plain: true }));
+
+    res.render('all', { events });
+});
 
 module.exports = router;
