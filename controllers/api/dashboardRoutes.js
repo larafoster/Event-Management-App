b0a5809a48f881486d04c363da5caca83afddb15
Cameron H <cameronheadlee6@gmail.com>
2021-08-18 14:02:16 -0400

    adding route to eventRoutes.js

--- controllers/api/dashboardRoutes.js
+++ controllers/api/dashboardRoutes.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const { Event } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', withAuth)
+
 
 module.exports = router;
