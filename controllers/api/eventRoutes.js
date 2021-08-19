e87131834f83f12ef2a2fb9d9f3c1075ef82837a
Cameron H <cameronheadlee6@gmail.com>
2021-08-19 10:20:43 -0400

    adding dashboardRoutes

--- controllers/api/eventRoutes.js
+++ controllers/api/eventRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Event } = require('../../models');
-const withAuth = require('../../utils/auth');
+//const withAuth = require('../../utils/auth');
 
 router.get('/', async (req, res) => {
     const eventData = await Event.findAll().catch((err) => {
@@ -11,6 +11,25 @@ router.get('/', async (req, res) => {
 
     res.render('all', { events });
 });
+
+/*router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const eventData = await Event.update({
+            ...req.body,
+            name: req.session.name,
+            description: req.session.description,
+            event_date: req.session.event_date,
+            event_time: req.session.event_time,
+            category: req.session.category,
+            covid_items: req.session.covid_items,
+            user_id: req.session.user_id,
+        });
+        res.status(200).json(eventData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});*/
+
 
 
 module.exports = router;
