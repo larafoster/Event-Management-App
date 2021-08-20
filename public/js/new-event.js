d08184368514f6431dbd439b0b52cbbd29ffb9af
Cameron H <cameronheadlee6@gmail.com>
2021-08-20 14:19:33 -0400

    where is dashboard

--- public/js/new-event.js
+++ public/js/new-event.js
@@ -1,10 +1,10 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#event-name').value.trim();
+  const name = document.querySelector('#name').value.trim();
   const category = document.querySelector('#category').value.trim();
   const description = document.querySelector('#description').value.trim();
-  const event_date = document.querySelector('#event-date').value.trim();
+  const event_date = document.querySelector('#event_date').value.trim();
   const event_time = document.querySelector('#event_time').value.trim();
   const covid_items = document.querySelector('#covid_items').value.trim();
 
