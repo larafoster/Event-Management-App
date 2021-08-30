285d2b820f2bee79dd71abb8ef0e7dc9592dde41
Gillian Baker <gillianbaker97@gmail.com>
2021-08-30 15:37:50 -0400

    adding in index

--- models/index.js
+++ models/index.js
@@ -1,27 +1,10 @@
 const User = require('./User');
 const Event = require('./Event');
-const Comment = require('./Comment');
-//const Join = require('./Join');
-
-User.belongsToMany(Event, {
-  through: {
-    model: Comment,
-    unique: false
-  },
-  as: 'planned_events'
-});
-
-Event.belongsToMany(User, {
-  through: {
-    model: Comment,
-    unique: false
-  },
-  as: 'event_goers'
-});
-
-/* Event.hasMany(Comment, {
+User.hasMany(Event, {
   foreignKey: 'user_id',
-  },
-); */
-
-module.exports = { User, Event, Comment };
+  onDelete: 'CASCADE'
+});
+Event.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+module.exports = { User, Event };
